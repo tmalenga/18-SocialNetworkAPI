ac77b9176573c1cf1dd526782109976183e08757
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-04-02 17:07:59 -0300

    update models

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validator(validEmail) {
+        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(
+            validEmail
+        );
+    },
+    message: "please enter a valid email"
+
+  },
+  thoughts: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought'
+      }
+    ],
+  friends: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+      }
+  ]
+  },
+  { 
+  toJSON: {},
+  id: false
+}
+);
+
+const User = model('User', UserSchema);
+
+module.exports = User;
