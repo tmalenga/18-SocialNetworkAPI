0d566cbb2e83b4d901a6ba31d0b7b3eef734b106
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-04-02 20:53:42 -0300

    Update for connection to db

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001
 
+const db = require('./config/connection');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialMediaAPI', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
 
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
