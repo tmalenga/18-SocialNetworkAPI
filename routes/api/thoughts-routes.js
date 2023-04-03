b6090a2dbb2215256f893a2ce450ec6cbb2600d7
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-04-02 22:54:26 -0300

    update routes

---
+++ routes/api/thoughts-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    addReaction,
+} = require('../../controllers/thoughtsController');
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought)
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+
+router
+    .route('/:thoughtId/:reactionId')
+    
+module.exports = router;
