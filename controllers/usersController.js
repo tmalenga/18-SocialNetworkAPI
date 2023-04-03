35e042dbc3888bfacbb89ef55708a09c2f0e8ab9
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-04-02 21:35:12 -0300

    update user controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -54,6 +54,39 @@ const userController = {
          .catch(err => res.json(err));
      },
 
+     updateUser({ params, body}, res) {
+         User.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true})
+         .then(dbUserData => {
+             if (!dbUserData) {
+                 res.status(404).json({ message: 'No user found with this ID!' });
+                 return;
+             }
+             res.json(dbUserData);
+         })
+            .catch(err => res.json(err))
+     },
+
+     deleteUser({ params }, res) {
+         User.findOneAndDelete({ _id: params.id })
+         .then(dbUserData => {
+         if (!dbUserData) {
+             res.status(404).json({ message: 'No user found with this ID!' });
+             return;
+         }
+         res.json(dbUserData);
+         })
+         .catch(err => res.status(400).json(err))
+     },
+
+     removeFriend( { params }, res) {
+         User.findOneAndUpdate(
+             { _id: params.userId },
+             { $pull: { friends: params.friendId }},
+             { new: true}
+         )
+         .then(dbUserData => res.json(dbUserData))
+         .catch(err => res.json(err));
+     }
      
 };
 
